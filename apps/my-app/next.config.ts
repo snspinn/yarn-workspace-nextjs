dc78ff579012f3ae0d191aa552e115b9397ef2e1
Samuel Spinn <sspinn@netfeasa.com>
2024-12-09 14:18:59 +0000

    Link workspaces & prove all is building

--- apps/my-app/next.config.ts
+++ apps/my-app/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  serverExternalPackage: ['server-package']
 };
 
 export default nextConfig;
