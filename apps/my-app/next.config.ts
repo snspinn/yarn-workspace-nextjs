7e151f50f1705a29feb4fb43d89dca9a0bc2e754
Samuel Spinn <sspinn@netfeasa.com>
2024-12-12 16:51:43 +0000

    Set resolve.symlinks = false. No-dice..

--- apps/my-app/next.config.ts
+++ apps/my-app/next.config.ts
@@ -2,7 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  serverExternalPackages: ['server-package']
+  serverExternalPackages: ['server-package'],
+  webpack(config) {
+    // Manually set node-loader. Seem mutually exclusive to 'serverExternalPackges'
+    // config.module.rules.push({
+		//   test: /\.server-package.linux-x64-gnu.node$/,
+		// 	loader: 'node-loader'
+		// });
+
+    config.resolve.symlinks = false;
+		return config;
+	}
 };
 
 export default nextConfig;
